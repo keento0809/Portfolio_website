be2516b5b6bac04fc6ab3547287dc69126e8e9de
KEENTO <handball0321@gmail.com>
2022-11-14 13:36:24 -0800

    Introduced contentful for my resume

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import Main from "./components/Main";
 import ToggleModeProvider from "./context/ToggleModeContext";
+import React from "react";
+import useContentful from "./hooks/useContentful";
 
 function App() {
+  const { getResume } = useContentful();
+  useEffect(() => {
+    getResume().then((res) =>
+      console.log("https:" + res[0].fields.myResume.fields.file.url)
+    );
+    // console.log(resume);
+  }, []);
   return (
     <ToggleModeProvider>
       <Main />
