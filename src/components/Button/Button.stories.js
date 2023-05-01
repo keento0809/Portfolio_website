a594331521abb00ccd04d070cd81ba45c159b75b
KEENTO <handball0321@gmail.com>
2023-05-01 12:40:40 +0900

    Introduced storybook

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+const Meta = {
+  component: Button,
+  title: "Button",
+};
+
+export default Meta;
+
+export const Default = {
+  args: {
+    url: "#myproject",
+    label: "Explore",
+  },
+};
